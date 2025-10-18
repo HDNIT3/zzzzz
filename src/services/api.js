394acef89e61c2312698ddc68c1ff14415962913
HDNIT3 <nguyenhuynh.663459@gmail.com>
2025-10-18 13:35:23 +0700

    sss

--- src/services/api.js
+++ src/services/api.js
@@ -1,20 +1,18 @@
-import axios from "axios";
+﻿import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080", 
-  headers: {
-    "Content-Type": "application/json",
-  },
+    baseURL: process.env.REACT_APP_API_URL || "http://localhost:8080",  // Sử dụng env var
+    headers: {
+        "Content-Type": "application/json",
+    },
 });
 
 api.interceptors.request.use((config) => {
-
-  const token = sessionStorage.getItem("token") || localStorage.getItem("token");
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
+    const token = sessionStorage.getItem("token") || localStorage.getItem("token");
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
 });
 
 export default api;
